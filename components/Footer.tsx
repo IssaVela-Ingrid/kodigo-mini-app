4196118d1bef97817c65f35c2ff1ff85744aea60
SoporteWeb <soporteweb@itcha.edu.sv>
2025-07-21 16:19:43 -0600

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,14 @@
 // components/Footer.tsx
-export default function Footer() {
-  const footerStyle = {
-    backgroundColor: '#222',
-    color: 'white',
-    textAlign: 'center',
-    padding: '20px',
-    marginTop: 'auto', // Esto ayuda a empujar el footer al final de la página si el contenido es corto
-    width: '100%',
-    boxShadow: '0 -2px 5px rgba(0,0,0,0.2)',
-    fontSize: '0.9em',
-  };
+import React from 'react'; // Asegúrate de importar React si no lo estás haciendo ya
 
+export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer style={footerStyle}>
-      <p>&copy; {year} Ingrid/Issa. Todos los derechos reservados.</p>
-      <p>Desarrollado con Next.js y Firebase.</p>
+    <footer className="bg-kodigo-dark text-kodigo-text-light text-center p-5 shadow-lg border-t border-kodigo-primary-green text-sm">
+      <p className="my-1">&copy; {year} Ingrid/Issa. Todos los derechos reservados.</p>
+      <p className="my-1">Desarrollado con Next.js y Firebase.</p>
+      <p className="my-1 text-kodigo-secondary-green">Estilo KODIGO Music</p>
     </footer>
   );
 }
